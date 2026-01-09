df904301a7c702a55688b06c49e32fe12da14672
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-10 00:16:44 +0200

    update

--- resources.tf
+++ resources.tf
@@ -1,21 +1,23 @@
 
+
 ###  ---  Default Template  ---  ###
 module "minio" {
   source = "./modules/minio"
   depends_on = [kubernetes_namespace.kubeai-system]
 }
 
+module "keda" {
+  source = "./modules/keda"
+  depends_on = [module.minio"]
+}
+
 module "events" {
   source = "./modules/events"
-  depends_on = [module.minio]
+  depends_on = [module.keda]
 }
 
 module "function" {
   source = "./modules/function"
-  depends_on = [module.events]
+  depends_on = [module.keda]
 }
 
-module "keda" {
-  source = "./modules/keda"
-  depends_on = [module.function"]
-}
